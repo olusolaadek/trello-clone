e10d9d630ca9ad490cffd8cfc21b9de6135b042d
Olusola Adekunle <olusola.adekunle@palpensions.com>
2022-09-05 13:40:20 +0100

    Provide dispatch through the context

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -6,6 +6,7 @@ import { useImmerReducer } from "use-immer";
 type AppStateContextProps = {
   lists: List[];
   getTasksByListId: (id: string) => Task[];
+  dispatch: Dispatch<Action>;
 };
 const AppStateContext = createContext<AppStateContextProps>(
   {} as AppStateContextProps
@@ -13,14 +14,16 @@ const AppStateContext = createContext<AppStateContextProps>(
 
 // export const AppStateProvider: FC = ({ children }) => {
 export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
-  const { lists } = appData;
+  const [state, dispatch] = useImmerReducer(appStateReducer, appData);
+
+  const { lists } = state;
 
   const getTasksByListId = (id: string) => {
     return lists.find((list) => list.id === id)?.tasks || [];
   };
 
   return (
-    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+    <AppStateContext.Provider value={{ lists, getTasksByListId, dispatch }}>
       {children}
     </AppStateContext.Provider>
   );
